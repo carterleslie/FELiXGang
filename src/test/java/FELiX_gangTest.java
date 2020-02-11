6a54df65f7e1c4e1c2da8696e53ff9feec4c3255
caleslie00 <42848797+caleslie00@users.noreply.github.com>
2020-02-11 08:59:54 -0600

    Initial Setup
    Setting up initial files to work with mvn test and mvn install

---
+++ src/test/java/FELiX_gangTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
+
+/******************************************************************************
+ *  Author : cal17b Carter Leslie
+ *  Class  : Spring 2020 CS374.01 Dr. Reeves
+ *  Date   : 
+ *  Task   : 
+ *
+ *  This is a file for creating other test files to use quickly in the future.
+ *
+ ******************************************************************************/
+
+public class FELiX_gangTest
+{
+    FELiX_gang sampleVar;
+
+    @Before
+    public void initialize()
+    {
+    	sampleVar = new FELiX_gang();
+    }
+
+    //@Test 
+    /*public void test**methodName**()
+    {
+    	try
+    	{
+    		*type** ans = sampleVar.**methodName(proper utilization)**;
+    		//assertEquals(**expected**,ans);
+    	}
+        catch (Exception e)
+        {
+            System.out.println("**tells if failed or passed**");
+        }
+    } 
+    */
+}

6dfdc32a2546714e1317158e42de42c803f2e965
UofMTigerFan <wsl15a@acu.edu>
2020-02-12 20:13:46 -0600

    Add files via upload
    Added test cases, commented out.

---
+++ FELiXGangTest.java
@@ -0,0 +1,81 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
+
+/******************************************************************************
+ *  Author : cal17b Carter Leslie, wsl15a Wade Linder, 
+ *  Class  : Spring 2020 CS374.01 Dr. Reeves
+ *  Date   : 
+ *  Task   : This is the test file for Project 1 of CS374. 
+ *
+ *  This is a file for creating other test files to use quickly in the future.
+ *
+ ******************************************************************************/
+
+public class FELiXGangTest
+{
+    FELiXGang sampleVar;
+
+    @Before
+    public void initialize()
+    {
+    	sampleVar = new FELiXGang();
+    }
+
+    //@Test 
+    /*testEmpty()
+    {
+    	try
+    	{
+    		string ans = sampleVar.**methodName(proper utilization)**;
+    		assertEquals("",ans);
+    	}
+        catch (Exception e)
+        {
+            System.out.println("Matrix is not empty.");
+        }
+    } 
+	
+	//@Test
+	testTeamSize()
+	{
+		try
+		{
+			int ans = sampleVar.**methodName(proper utilization)**;
+			assertEquals(3,ans);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Teams are not three deep.");
+		}
+	}
+	
+	//@Test
+	testTeamNumber()
+	{
+		try
+		{
+			int ans = sampleVar.**methodName(proper utilization)**;
+			assertEquals(5,ans);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Not right amount of teams.");
+		}
+	}
+	
+	//@Test
+	testHappiness()
+	{
+		try
+		{
+			double ans = sampleVar.**methodName(proper utilization)**;
+			assertEquals(5,ans);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Not happy enough.");
+		}
+	}
+    */
+}

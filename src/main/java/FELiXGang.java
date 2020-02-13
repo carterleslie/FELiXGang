f0f9cdee7ad917cba20f98feb329c6a697dd2005
UofMTigerFan <wsl15a@acu.edu>
2020-02-12 21:03:18 -0600

    Add files via upload
    Added printTeams function that prints out the teams line by line, starting from first in the team and going down the team then shifting to the next column.

--- src/main/java/FELiXGang.java
+++ src/main/java/FELiXGang.java
@@ -1,12 +1,12 @@
 /******************************************************************************
-*  Authors: cal17b Carter Leslie, wsl15a Wade Linder,
-*  Class  : Spring 2020 CS374.01 Dr. Reeves
-*  Date   : 2/21/2020
-*  Task   : Project 1 - Happy Teams
-*
-*  Team FELiXGang's main class for the Happy Teams Project.
-*
-******************************************************************************/
+ *  Author : cal17b Carter Leslie, wsl15a Wade Linder
+ *  Class  : Spring 2020 CS374.01 Dr. Reeves
+ *  Date   : 
+ *  Task   : 
+ *
+ *  This is a file for creating other files to use quickly in the future.
+ *
+ ******************************************************************************/
 
 public class FELiXGang 
 {
@@ -25,5 +25,23 @@ public class FELiXGang
     	else
     		classArray = new classArray[teamSize][classSize/teamSize +1];
     }
-
+	
+	//Prints out created teams.
+	public void printTeams() {
+		int numTeams = classSize/teamSize;
+		System.out.println((for int c = 0; i < numTeams; c++) {
+			System.out.println("Team "i+1 ": "); 
+			(for int r = 0; i< teamSize; r++) {
+				if(r < teamSize-1)
+					System.out.println(classArray[r][c] ", ");	
+				else
+					System.out.println(classArray[r][c]);
+			}
+			System.out.println("\n");
+		} );
+	}
+	
+	public void happinessAvg() {
+		
+	}
 }

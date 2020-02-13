65f7a27e0c65c3f123d2d3e3a9e1e860dd16ddf0
caleslie00 <42848797+caleslie00@users.noreply.github.com>
2020-02-12 23:42:15 -0600

    Big Changes
    Renamed lots of stuff to better fit what it is(Array->Matrix).
    Added variables that were needed in mutliple functions(numTeams).
    Added a String Matrix to hold all of the preferences(prefsArray).
    Set up the initializer to create and fill the matrix from the sample file.
    Changed test main to fit the provided case.
    Added lots of new functions(all commented).

--- src/main/java/FELiXGang.java
+++ src/main/java/FELiXGang.java
@@ -10,59 +10,122 @@
 
 import java.io.File; 
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class FELiXGang 
 {
-	private int classSize;
-	private int teamSize;
-	private String[][] classArray;
+	private int classSize; //size of the entire "class" of students
+	private int teamSize; //size of the teams, designated by user
+	private int numTeams; //number of teams, calculated with the above two ints in the initializer
+	private String[][] teamsMatrix; //the matrix of all names of the team members in respective teams
+	private String[][] prefsMatrix; //the matrix of all prefs of the team members, parallel to teamsMatrix
 
     //initializer for a FELiXGang
     public FELiXGang(int cSize, int tSize)
     {
     	classSize = cSize;
     	teamSize = tSize;
-    	if(classSize % teamSize == 0)
-    		classArray = new String[teamSize][classSize / teamSize];
-    	else
-    		classArray = new String[teamSize][classSize / teamSize +1];
-    }
 
+    	if(classSize % teamSize == 0)
+    		numTeams = classSize / teamSize;
+    	else
+    		numTeams = classSize / teamSize + 1;
+    	teamsMatrix = new String[teamSize][numTeams];
+    	prefsMatrix = new String[teamSize][numTeams];
+    	fillTeams("sampleTeam.txt");
+    }
+    //gets the teamSize
     public int getTeamSize()
     {
     	return teamSize;
     }
-
-    public int getClassSize()
+    //gets the classSize
+    public int getclassSize()
     {
     	return classSize;
     }
+    //gets the numTeams
+    public int getNumTeams()
+    {
+    	return numTeams;
+    }
+    //fills up the teamsMatrix and prefsMatrix with whatever file you pass it
+    //fills them in a way that only leaves the end index of a column open as null when there aren't
+    //enough people in the class.
+    //fills all of [0][0-numTeams], then [1][0-numTeams] such that only the far right of the bottom row is open at the end
+    private void fillTeams(String fileName)
+    {
+    	File file = new File(fileName);
+    
+    	try 
+    	{
+        	Scanner sc = new Scanner(file);
+        	int r = 0, c = 0;
 
-    public void fillTeams()
+	    	while (sc.hasNextLine()) 
+	     	{
+	     		String nextLine = sc.nextLine();
+	     		String nameAndPrefs[] = nextLine.split(",",2);
+
+	     		fillTeamsMatrixIndex(nameAndPrefs[0], r, c);
+	     		if(nextLine.indexOf(',') >= 0)
+	     			fillPrefsMatrixIndex(nameAndPrefs[1], r, c);
+	     		else
+	     			fillPrefsMatrixIndex("0,0,0,0,0,0", r, c);
+	     		if(c < numTeams-1)
+	     			c++;
+	     		else
+	     		{
+	     			r++;
+	     			c = 0;
+	     		}
+	     	}
+	     	sc.close();
+    	} 
+    	catch (FileNotFoundException e) 
+    	{
+        	e.printStackTrace();
+    	}
+    }
+    //adds name to teamsMatrix[r][c]
+    public void fillTeamsMatrixIndex(String name, int r, int c)
+    {
+    	teamsMatrix[r][c] = name;
+    }
+	//gets the name at teamsMatrix at index [r][c]
+    public String getTeamsMatrixIndex(int r, int c)
+    {
+    	return teamsMatrix[r][c];
+    }
+	//adds prefs to prefsMatrix at index [r][c]
+    public void fillPrefsMatrixIndex(String prefs, int r, int c)
+    {
+    	prefsMatrix[r][c] = prefs;
+    }
+	//gets the prefs at prefsMatrix at index [r][c]
+    public String getPrefsMatrixIndex(int r, int c)
+    {
+    	return prefsMatrix[r][c];
+    }
 
 	//Prints out created teams.
 	public void printTeams() {
-		int numTeams;
-		if(classSize % teamSize == 0)
-			numTeams = classSize/teamSize;
-		else
-			numTeams = classSize/teamSize +1;
 		for (int c = 0; c < numTeams; c++) {
 			System.out.print("Team " + (c+1) + ": "); 
-			for (int r = 0; r< teamSize; r++) {
+			for (int r = 0; r < teamSize; r++) {
 				if(r < teamSize-1)
-					System.out.print(classArray[r][c] + ", ");	
+					System.out.print(teamsMatrix[r][c] + ", ");	
 				else
-					System.out.println(classArray[r][c]);
+					System.out.println(teamsMatrix[r][c]);
 			}
 			System.out.println("");
 		}
 	}
 	
-	/*public static void main( String[] args )
+	public static void main( String[] args )
     {
         System.out.println( "Hello, this is a sample file!" );
-        FELiXGang t = new FELiXGang(15,3);
+        FELiXGang t = new FELiXGang(4,2);
         t.printTeams();
-    }*/
+    }
 }

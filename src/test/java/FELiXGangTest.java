742bf0b0b7ac5ef800d6c69e475d6bac92d64fa1
UofMTigerFan <wsl15a@acu.edu>
2020-02-18 20:46:27 -0600

    Update FELiXGangTest.java
    Small update to last commit

--- src/test/java/FELiXGangTest.java
+++ src/test/java/FELiXGangTest.java
@@ -30,6 +30,69 @@ public class FELiXGangTest
     		System.out.println("Starting test: " + description.getMethodName());
     	    }
     	};
+/*
+    FELiXGang sampleVar;
+
+    @Before
+    public void initialize()
+    {
+    	sampleVar = new FELiXGang(4,2);
+    }
+*/
+    @Test
+    public void testInizializer()
+    {
+    	FELiXGang test1 = new FELiXGang(15,3);
+		assertTrue(test1.getTeamSize() == 3); //team size should be 3
+
+    	assertTrue(test1.getClassSize() == 15); //class size should be 15
+
+    	assertTrue(test1.getNumTeams() == 5); //num teams should be 5
+
+        
+    	FELiXGang test2 = new FELiXGang(14,3);
+		assertTrue(test2.getTeamSize() == 3); //team size should be 3
+
+		assertTrue(test2.getClassSize() == 14); //class size should be 14
+
+		assertTrue(test2.getNumTeams() == 5); //num teams should be 5
+
+    	FELiXGang test3 = new FELiXGang(13,3);
+		assertTrue(test3.getTeamSize() == 3); //team size should be 3
+
+    	assertTrue(test3.getClassSize() == 13); //class size should be 14
+
+		assertTrue(test3.getNumTeams() == 5); //num teams should be 5
+    }
+
+    @Test 
+    public void testFillMatrix()
+    {
+		FELiXGang test1 = new FELiXGang(4,2);
+		String test = "help";
+		test1.fillTeamsMatrixIndex(test,0,0);
+    	String ans = test1.getTeamsMatrixIndex(0,0);
+    	assertEquals("help",ans);
+
+    	String ans = "Natasha";
+		assertEquals(ans,test1.getTeamsMatrixIndex(1,0));
+    } 
+	
+	@Test
+	public void testHappiness()
+	{
+		FELiXGang test1 = new FELiXGang(4,2);
+
+        int val = test1.getIndividualHappinessMatrixIndex(0,0); //should be 5
+        int ans = 13;
+        assertEquals(ans,val);
+
+        int val2 = test1.getTeamHappinessIndex(0); // should be (5 + 6) / 2, which is 5 lol
+        int ans2 = 14; //(13 + 16) / 2 = 14
+        assertEquals(ans2,val2);
+	}
+    
+}
 
     FELiXGang sampleVar;
 
